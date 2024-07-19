f7bf89f2d31f478db3df6ac1191dcee97124ea16
genose.org <genose@genose.org>
2024-07-19 17:29:54 +0200

    EventController fix Routes for Events ID and Groups List + ID

--- src/main/java/fr/olprog_c/le_phare_culturel/controllers/routes/RouteDefinition.java
+++ src/main/java/fr/olprog_c/le_phare_culturel/controllers/routes/RouteDefinition.java
@@ -26,8 +26,8 @@ public final class RouteDefinition {
     public static class Events {
         public static final String EVENTS_URL = "/events";
         public static final String EVENTS_WITH_ID_URL = EVENTS_URL+"/{eventid}";
-        public static final String EVENTS_WITH_ID_GROUP_WITH_ID_URL = EVENTS_URL+"/{eventid}/"+Groups.GROUPS_WITH_ID_URL;
-        public static final String EVENTS_WITH_ID_GROUP_LIST_URL= EVENTS_URL+"/{eventid}/"+Groups.GROUPS_URL;
+        public static final String EVENTS_WITH_ID_GROUP_WITH_ID_URL = EVENTS_URL+"/{eventid}"+Groups.GROUPS_WITH_ID_URL;
+        public static final String EVENTS_WITH_ID_GROUP_LIST_URL= EVENTS_URL+"/{eventid}"+Groups.GROUPS_URL;
 
         public static final String TAGS_URL = EVENTS_URL + "/tags/{filters}";
         public static final String TAGS_FILTER_URL = EVENTS_URL + "/filters/{tags}";
