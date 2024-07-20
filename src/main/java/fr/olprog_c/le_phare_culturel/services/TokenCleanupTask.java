8e049f671eaa74f9a965f59bff1e091e2bc43770
Samuel MICHAUX - OlProG <samuel.michaux@ik.me>
2024-07-20 19:52:17 +0200

    Clean code and add environment for expiration time of tokens
    J'ai ajouté des variables d'environnements pour l'expiration des tokens.
    Ils sont réglés par défaut, le temps du dev, à 1 et 2 min.
    Si vous le souhaitez, vous pouvez ajouter dans le .env ces 2 valeurs
    avec des temps de minutes différentes

--- src/main/java/fr/olprog_c/le_phare_culturel/services/TokenCleanupTask.java
+++ src/main/java/fr/olprog_c/le_phare_culturel/services/TokenCleanupTask.java
@@ -2,26 +2,23 @@ package fr.olprog_c.le_phare_culturel.services;
 
 import java.time.Instant;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import fr.olprog_c.le_phare_culturel.repositories.BlacklistedTokenRepository;
-import fr.olprog_c.le_phare_culturel.repositories.RefreshTokenRepository;
 
 @Component
 public class TokenCleanupTask {
 
-  @Autowired
-  private RefreshTokenRepository refreshTokenRepository;
+  private final BlacklistedTokenRepository blacklistedTokenRepository;
 
-  @Autowired
-  private BlacklistedTokenRepository blacklistedTokenRepository;
+  public TokenCleanupTask(BlacklistedTokenRepository blacklistedTokenRepository) {
+    this.blacklistedTokenRepository = blacklistedTokenRepository;
+  }
 
   @Scheduled(cron = "0 0 0 * * ?") // tous les jours à minuit
   public void cleanUpExpiredTokens() {
     Instant now = Instant.now();
-    refreshTokenRepository.deleteAll(refreshTokenRepository.findAllByExpiryDateBefore(now));
     blacklistedTokenRepository.deleteAll(blacklistedTokenRepository.findAllByExpiryDateBefore(now));
   }
 }
