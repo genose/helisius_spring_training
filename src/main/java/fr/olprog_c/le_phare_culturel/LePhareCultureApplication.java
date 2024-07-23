cd2595b6a95978994991e41b78f3a62f12dd88b1
Samuel MICHAUX <samuel.michaux@ik.me>
2024-07-23 13:41:32 +0200

    ajout de quelques datas

--- src/main/java/fr/olprog_c/le_phare_culturel/LePhareCultureApplication.java
+++ src/main/java/fr/olprog_c/le_phare_culturel/LePhareCultureApplication.java
@@ -1,47 +1,15 @@
 package fr.olprog_c.le_phare_culturel;
 
-import java.util.List;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
-
-import fr.olprog_c.le_phare_culturel.api.models.Event;
-import fr.olprog_c.le_phare_culturel.api.services.APIEventService;
-import fr.olprog_c.le_phare_culturel.repositories.EventRepository;
 
 @SpringBootApplication
 @EnableAsync
 public class LePhareCultureApplication {
 
-  private final APIEventService apiEventService;
-  private final EventRepository eventRepository;
-
-  public LePhareCultureApplication(APIEventService apiEventService, EventRepository eventRepository) {
-    this.apiEventService = apiEventService;
-    this.eventRepository = eventRepository;
-  }
-
-  public static void main(String[] args) {
-    SpringApplication.run(LePhareCultureApplication.class, args);
-  }
-
-  @Bean
-  public CommandLineRunner run() {
-    return args -> {
-      if (eventRepository.count() > 0) {
-        System.out.println("Events already fetched from API");
-        return;
-      }
-      System.out.println("Fetching events from API....");
-      apiEventService.fetchEvents().subscribe(eventResponse -> {
-        List<Event> events = List.of(eventResponse.events());
-        apiEventService.save(events);
-      });
-
-    };
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(LePhareCultureApplication.class, args);
+    }
 
 }
---
+++ src/main/java/fr/olprog_c/le_phare_culturel/initialise/GroupDataInitializer.java
@@ -0,0 +1,57 @@
+package fr.olprog_c.le_phare_culturel.initialise;
+
+import java.time.Instant;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import fr.olprog_c.le_phare_culturel.entities.EventEntity;
+import fr.olprog_c.le_phare_culturel.entities.EventGroupUserEntity;
+import fr.olprog_c.le_phare_culturel.entities.UserEntity;
+import fr.olprog_c.le_phare_culturel.repositories.EventRepository;
+import fr.olprog_c.le_phare_culturel.repositories.GroupRepository;
+import fr.olprog_c.le_phare_culturel.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Component
+@Order(3)
+public class GroupDataInitializer implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+    private final EventRepository eventRepository;
+    private final GroupRepository groupRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        try {
+            UserEntity user1 = userRepository.findByEmail("john-wick@local.com").orElseThrow();
+            UserEntity user2 = userRepository.findByEmail("john-doe@local.fr").orElseThrow();
+
+            EventEntity event1 = eventRepository.findRandomEvent().orElseThrow();
+            EventEntity event2 = eventRepository.findRandomEvent().orElseThrow();
+
+            EventGroupUserEntity group1 = new EventGroupUserEntity();
+            group1.setGroupName("The Hitmen");
+            group1.setDescription("A group of professional hitmen");
+            group1.setGroupSize(5);
+            group1.setReferencedUserAuthor(user1);
+            group1.setRelatedEvents(event1);
+            group1.setTimeMeet(Instant.now().plusSeconds(3600));
+            groupRepository.save(group1);
+
+            EventGroupUserEntity group2 = new EventGroupUserEntity();
+            group2.setGroupName("Test Group");
+            group2.setDescription("A group of professional hitmen");
+            group2.setGroupSize(10);
+            group2.setReferencedUserAuthor(user2);
+            group2.setRelatedEvents(event2);
+            group2.setTimeMeet(Instant.now().plusSeconds(3600));
+            groupRepository.save(group2);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+    }
+}
